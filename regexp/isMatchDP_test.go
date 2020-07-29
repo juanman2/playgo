84c3b77d54fcef179c3bcef3c9f2bed6c04acfd0
Juan Tellez <juan@Juans-MacBook-Pro-2.local>
2020-07-28 19:50:50 -0700

    add improvements to DP regexp implementation including + support

--- regexp/isMatchDP_test.go
+++ regexp/isMatchDP_test.go
@@ -30,3 +30,41 @@ func TestIsRegexpMatch(t *testing.T) {
 		}
 	}
 }
+
+func TestIsRegexpMatchPlus(t *testing.T) {
+	tests := []struct {
+		inS string
+		inP string
+		out bool
+	}{
+		{"aaca", ".", false},
+		{"aacb", ".*", true},
+		{"aacc", "a*.c", true},
+		{"aa", "a", false},
+		{"mississippi", "mis*is*ip*.", true},
+		{"aaa", "a*a", true},
+		{"ab", ".*c", false},
+		{"aaca", "a+", false},
+		{"aacb", "a+cb", true},
+		{"aacc", "a+.c", true},
+		{"aa", "a", false},
+		{"mississippi", "mis+is*ip+.", true},
+		{"aaa", "a+a", true},
+		{"aaa", "a*", true},
+		{"aaa", "a+", true},
+		{"ab", "a+c", false},
+		{"ac", "a+c", true},
+		{"b", "a+b", false},
+		{"b", "a*b", true},
+	}
+
+	for _, tt := range tests {
+		match := IsRegexpMatchDPPlus(tt.inS, tt.inP)
+		fmt.Printf("IsRegexpMatchDPPlus(%s, %s) returned %v expected %v\n",
+			tt.inS, tt.inP, match, tt.out)
+		if match != tt.out {
+			t.Errorf("IsRegexpMatch(%s, %s) returned %v expected %v",
+				tt.inS, tt.inP, match, tt.out)
+		}
+	}
+}
